ae2227f22057b0529814c6992dff50e2b9198a6f
samy100 <samuel.matos@upr.edu>
2014-12-05 01:44:22 -0500

    Small Fixes

--- route.py
+++ route.py
@@ -1,62 +1,84 @@
 import os
 import subprocess
+import shutil
 
 # Detect current IP address.
 cmd = "/sbin/ifconfig | grep \'inet addr\'"
-p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-p.wait()
+p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+print cmd
+print p.wait()
 (output, error) = p.communicate()
+print (output, error), "\n"
 rawIPList = output.split()
 modIPList = []
 myIP = ""
 
 for ip in rawIPList:
-	if 'addr:' in ip:
-		modIPList.append(ip[5:])
+    if 'addr:' in ip:
+        modIPList.append(ip[5:])
 
 for ip in modIPList:
-	if ip != '127.0.0.1':
-		myIP = ip
+    if ip != '127.0.0.1':
+        myIP = ip
 
+print "IP is " + myIP + "\n"
 
 # Add Listener to apache2 service for IP.
-f = open("/etc/apache2/ports.conf", 'r+')
-f.write("NameVirtualHost *:80\n")
-f.write("Listen " + myIP + ":80")
+virtHost = "NameVirtualHost *:80"
+listener = "Listen " + myIP + ":80"
+portConf = "# If you just change the port or add more ports here, you will likely also\n# have to change the VirtualHost statement in\n# /etc/apache2/sites-enabled/000-default\n# This is also true if you have upgraded from before 2.2.9-3 (i.e. from\n# Debian etch). See /usr/share/doc/apache2.2-common/NEWS.Debian.gz and\n# README.Debian.gz\n\n" + virtHost + "\n" + listener + "\n\n<IfModule mod_ssl.c>\n# If you add NameVirtualHost *:443 here, you will also have to change\n# the VirtualHost statement in /etc/apache2/sites-available/default-ssl\n# to <VirtualHost *:443>\n# Server Name Indication for SSL named virtual hosts is currently not\n# supported by MSIE on Windows XP.\nListen 443\n</IfModule>\n\n<IfModule mod_gnutls.c>\nListen 443\n</IfModule>\n\n"
+path = "/etc/apache2/ports.conf"
+bak = "/etc/apache2/ports.conf.bak"
 
+if os.path.isfile(path):
+    if not os.path.isfile(bak):
+        shutil.copyfile(path, bak)
+    f = open(path, "w")
+    f.write(portConf)
+    f.close()
+    del f
 
 # Restart apache service.
-cmd = "/etc/init.d/apache2 restart"
-p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-p.wait()
-
+cmd = "service apache2 restart"
+p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+print cmd
+print p.wait()
+print p.communicate(), "\n"
 
 # Enable iprouting.
 cmd = "echo 1 > /proc/sys/net/ipv4/ip_forward"
-p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-p.wait()
-
+p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+print cmd
+print p.wait()
+print p.communicate(), "\n"
 
 # Configure routing table for the first domain IP.
 cmd = "iptables -t nat -A PREROUTING -d 31.13.69.1/24 -j DNAT --to-destination " + myIP
-p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-p.wait()
+p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+print cmd
+print p.wait()
+print p.communicate(), "\n"
 
 # Configure routing table for the second domain IP.
 cmd = "iptables -t nat -A PREROUTING -d 31.13.70.1/24 -j DNAT --to-destination " + myIP
-p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-p.wait()
+p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+print cmd
+print p.wait()
+print p.communicate(), "\n"
 
 # Configure routing table for the third domain IP.
 cmd = "iptables -t nat -A PREROUTING -d 31.13.71.1/24 -j DNAT --to-destination " + myIP
-p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-p.wait()
+p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+print cmd
+print p.wait()
+print p.communicate(), "\n"
 
 # Configure the routing table to accept incoming traffic on port 80.
 cmd = "iptables -A INPUT -p tcp --dport 80 -j ACCEPT"
-p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-p.wait()
-
+p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+print cmd
+print p.wait()
+print p.communicate(), "\n"
 
 # Workflow
 # 1. Need to add "Listen <MY_IP>:80" to file /etc/apache2/ports.conf with other Listen statements at top 
